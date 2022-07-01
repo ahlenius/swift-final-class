c3864c223b59eff4774532beafccb12d737bdad4
GitHub Action <action@github.com>
2022-07-01 07:14:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/ahlenius/swiftfinalclass/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ahlenius/swiftfinalclass/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ahlenius.swiftfinalclass.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ahlenius.swiftfinalclass.MyBundle
 
 class MyApplicationService {
 
